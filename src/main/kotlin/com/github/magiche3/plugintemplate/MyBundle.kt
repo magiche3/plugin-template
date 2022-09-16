93b045aa6cc2f94095b486768717ba64a9f7651f
GitHub Action <action@github.com>
2022-09-16 02:45:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/magiche3/plugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/magiche3/plugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.magiche3.plugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/magiche3/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/magiche3/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.magiche3.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.magiche3.plugintemplate.MyBundle
 
 class MyApplicationService {
 
